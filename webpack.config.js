7d35252cc619dfc5edd82dc741d766d655d2a54c
Brook Li <bli@stellarservices.com>
2019-10-16 17:30:08 -0400

    Initial commit. Set up basic react-arcgis example.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join('src', 'index.jsx'),
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};

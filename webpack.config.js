f39a460b47983f5e5b2f49e90e11464a853e6a2b
Brook Li <bli@stellarservices.com>
2019-10-17 17:33:50 -0400

    Switched to webpack-dev-server and made some optimizations.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: path.join(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.join(__dirname, 'public'),
@@ -10,6 +11,9 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+  },
   module: {
     rules: [
       {

3eebcd46f3d101837e60d1e286ab15a516cde561
Brook Li <bli@stellarservices.com>
2019-10-21 10:58:31 -0400

    Got custom map view to work by passing view properties to Map component.

--- src/app.jsx
+++ src/app.jsx
@@ -12,95 +12,68 @@ export default class App extends Component {
   }
 
   async handleMapLoad(map, view) {
-    const [FeatureLayer] = await loadModules(
+    const [FeatureLayer, GraphicsLayer, Graphic] = await loadModules(
       [
         'esri/layers/FeatureLayer',
+        'esri/layers/GraphicsLayer',
+        'esri/Graphic',
       ],
     );
 
-    var popupTrailheads = {
-      "title": "{TRL_NAME}",
-      "content": "<b>City:</b> {CITY_JUR}<br><b>Cross Street:</b> {X_STREET}<br><b>Parking:</b> {PARKING}<br><b>Elevation:</b> {ELEV_FT} ft"
-    }
-
-    var trailheads = new FeatureLayer({
+    // Reference the feature layer to query
+    var featureLayer = new FeatureLayer({
       url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads_Styled/FeatureServer/0",
-      outFields: ["TRL_NAME","CITY_JUR","X_STREET","PARKING","ELEV_FT"],
-      popupTemplate: popupTrailheads
     });
 
-    map.add(trailheads);
+    // Layer used to draw graphics returned
+    var graphicsLayer = new GraphicsLayer();
+    map.add(graphicsLayer);
 
-    var popupTrails = {
-      "title": "Trail Information",
-      "content": function(){
-         return "This is {TRL_NAME} with {ELEV_GAIN} ft of climbing.";
-      }
-    }
-
-    var trails = new FeatureLayer({
-      url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails_Styled/FeatureServer/0",
-      outFields: ["TRL_NAME","ELEV_GAIN"],
-      popupTemplate: popupTrails
-    });
-
-    map.add(trails,0);
-
-    var popupOpenspaces = {
-      "title": "{PARK_NAME}",
-      "content": [{
-        "type": "fields",
-        "fieldInfos": [
-          {
-            "fieldName": "AGNCY_NAME",
-            "label": "Agency",
-            "isEditable": true,
-            "tooltip": "",
-            "visible": true,
-            "format": null,
-            "stringFieldOption": "textbox"
+    function addGraphics(result) {
+      graphicsLayer.removeAll();
+      result.features.forEach(function(feature){
+        var g = new Graphic({
+          geometry: feature.geometry,
+          attributes: feature.attributes,
+          symbol: {
+           type: "simple-marker",
+            color: [0,0,0],
+            outline: {
+             width: 2,
+             color: [0,255,255],
+           },
+            size: "20px"
           },
-          {
-            "fieldName": "TYPE",
-            "label": "Type",
-            "isEditable": true,
-            "tooltip": "",
-            "visible": true,
-            "format": null,
-            "stringFieldOption": "textbox"
-          },
-          {
-            "fieldName": "ACCESS_TYP",
-            "label": "Access",
-            "isEditable": true,
-            "tooltip": "",
-            "visible": true,
-            "format": null,
-            "stringFieldOption": "textbox"
-          },
-          {
-            "fieldName": "GIS_ACRES",
-            "label": "Acres",
-            "isEditable": true,
-            "tooltip": "",
-            "visible": true,
-            "format": {
-              "places": 2,
-              "digitSeparator": true
-            },
-            "stringFieldOption": "textbox"
+          popupTemplate: {
+           title: "{TRL_NAME}",
+           content: "This a {PARK_NAME} trail located in {CITY_JUR}."
           }
-        ]
-      }]
+        });
+        graphicsLayer.add(g);
+      });
     }
 
-    var openspaces = new FeatureLayer({
-      url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Parks_and_Open_Space_Styled/FeatureServer/0",
-      outFields: ["TYPE","PARK_NAME", "AGNCY_NAME","ACCESS_TYP","GIS_ACRES"],
-      popupTemplate: popupOpenspaces
+    function queryFeatureLayer(point, distance, spatialRelationship, sqlExpression) {
+      var query = {
+        geometry: point,
+        distance: distance,
+        spatialRelationship: spatialRelationship,
+        outFields: ["*"],
+        returnGeometry: true,
+        where: sqlExpression
+      };
+      featureLayer.queryFeatures(query).then(function(result) {
+        addGraphics(result, true);
+      });
+    }
+
+    view.when(function(){
+      queryFeatureLayer(view.center, 1500, "intersects");
     });
 
-    map.add(openspaces,0);
+    view.on("click", function(event){
+      queryFeatureLayer(event.mapPoint, 1500, "intersects");
+    });
 
     this.setState({
       map,
@@ -114,6 +87,10 @@ export default class App extends Component {
         <Map
           mapProperties={{
             basemap: 'topo-vector',
+          }}
+          viewProperties={{
+            center: [-118.80500, 34.02700],
+            zoom: 13,
           }}
           loaderOptions={{ css: true }}
           onLoad={this.handleMapLoad}
